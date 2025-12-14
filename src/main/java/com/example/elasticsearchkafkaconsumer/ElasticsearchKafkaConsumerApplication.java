dd72b9332d0cae2b77b8f40c08dcc28a5fa6c99c
wkc1002 <wkc1002@naver.com>
2025-12-14 20:45:40 +0900

    init2

---
+++ src/main/java/com/example/elasticsearchkafkaconsumer/ElasticsearchKafkaConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.elasticsearchkafkaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticsearchKafkaConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchKafkaConsumerApplication.class, args);
+    }
+
+}
