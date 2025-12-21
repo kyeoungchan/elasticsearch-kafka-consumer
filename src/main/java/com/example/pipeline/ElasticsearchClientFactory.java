e55f7f1f276102826d8171a0e8b353012f1ef19c
wkc1002 <wkc1002@naver.com>
2025-12-21 17:56:13 +0900

    feat: Elasticsearch Kafka Consumer 드디어 동작

--- src/main/java/com/example/pipeline/ElasticsearchClientFactory.java
+++ src/main/java/com/example/pipeline/ElasticsearchClientFactory.java
@@ -1,18 +1,24 @@
 package com.example.pipeline;
 
-import co.elastic.clients.elasticsearch.ElasticsearchAsyncClient;
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
 import co.elastic.clients.json.jackson.JacksonJsonpMapper;
 import co.elastic.clients.transport.ElasticsearchTransport;
 import co.elastic.clients.transport.rest_client.RestClientTransport;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import javax.net.ssl.SSLContext;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
 import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.elasticsearch.client.RestClient;
 
 public class ElasticsearchClientFactory {
 
-    public static ElasticsearchAsyncClient create(ElasticSearchSinkConnectorConfig config) {
+    public static ElasticsearchClient create(ElasticSearchSinkConnectorConfig config) {
         BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
         credentialsProvider.setCredentials(
                 AuthScope.ANY,
@@ -23,15 +29,28 @@ public class ElasticsearchClientFactory {
         );
 
         RestClient restClient = RestClient.builder(
-                        new HttpHost(config.getString(config.ES_CLUSTER_HOST), config.getInt(config.ES_CLUSTER_PORT))
+                        new HttpHost(config.getString(config.ES_CLUSTER_HOST), config.getInt(config.ES_CLUSTER_PORT), "https")
                 )
-                .setHttpClientConfigCallback(httpClientBuilder ->
-                        httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider)
-                )
+                .setHttpClientConfigCallback(httpClientBuilder -> {
+                    // SSL 검증을 우회하는 SSLContext 생성
+                    SSLContext sslContext = null;
+                    try {
+                        sslContext = SSLContextBuilder.create()
+                                .loadTrustMaterial(null, (chain, authType) -> true)
+                                .build();
+                    } catch (NoSuchAlgorithmException | KeyManagementException | KeyStoreException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                    return httpClientBuilder
+                            .setSSLContext(sslContext)
+                            .setSSLHostnameVerifier(NoopHostnameVerifier.INSTANCE) // 호스트명 검증 비활성화
+                            .setDefaultCredentialsProvider(credentialsProvider);
+                })
                 .build();
 
         ElasticsearchTransport transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
 
-        return new ElasticsearchAsyncClient(transport);
+        return new ElasticsearchClient(transport);
     }
 }
